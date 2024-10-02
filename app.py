3fa81cf4631333f211a26e595db1308d8e1dfd7f
sengvang.blo <skvgithub@gmail.com>
2024-10-02 14:11:16 +0700

    commit 02/10/2024 14:11:16.22

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello, Python is working!")
+from flask import Flask, render_template
+
+# Create a Flask app instance
+app = Flask(__name__)
+
+# Define a route for the homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
